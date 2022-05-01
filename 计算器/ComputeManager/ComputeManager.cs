8e28c131c58e87554f78e85f78a8bc087fe1ad1f
Darknights1108 <ansonangzhenloong@gmail.com>
2022-05-01 21:11:58 +0800

    扩展ComputeManage 和提取逻辑

---
+++ 计算器/ComputeManager/ComputeManager.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 计算器.ComputeManager
+{
+    internal class ComputeManager
+    {
+        /// <summary>
+        /// 文本解析
+        /// </summary>
+        /// <param name="currentInput">当前输入字符 一般为当前触发按键</param>
+        public void TextInput(string currentInput)
+        {
+            string text = "";
+            foreach (KeyValueModel keyValue in keyValuePairs)
+            {
+                text += keyValue.ToString();
+            }
+
+            if (currentNum != 0)
+            {
+                text += currentNum.ToString();
+            }
+            else if (currentNum == 0 && currentInput == "0")
+            {
+                text += "0";
+            }
+
+            if (text == "")
+            {
+                text = "0";
+            }
+
+            TB_1.Text = text;
+        }
+
+        /// <summary>
+        /// 数值参数
+        /// </summary>
+        /// <param name="input"></param>
+        public void HandleInput(string input)
+        {
+            switch (input)
+            {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    currentNum = currentNum * 10 + Convert.ToInt64(input);
+                    break;
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    HandleSymbol(input);
+                    break;
+                default:
+                    break;
+            }
+            TextInput(input);
+            beforeInput = input;
+        }
+
+        /// <summary>
+        /// 字符参数
+        /// </summary>
+        /// <param name="symbol"></param>
+        private void HandleSymbol(string symbol)
+        {
+            if ((beforeInput == "+" || beforeInput == "-" || beforeInput == "*" || beforeInput == "/") && currentNum == 0)
+            {
+                keyValuePairs.Last().Key = symbol;
+            }
+            else
+            {
+                keyValuePairs.Add(new KeyValueModel() { Key = symbol, Value = currentNum });
+                currentNum = 0;
+            }
+        }
+
+        /// <summary>
+        /// 四则运算处理
+        /// </summary>
+        /// <param name="symbol"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        private long Eq(string symbol, long a, long b)
+        {
+            if (symbol == "/" && b == 0)
+            {
+                throw new Exception("不能除0!");
+            }
+            switch (symbol)
+            {
+                case "*": return a * b;
+                case "/": return a / b;
+                case "+": return a + b;
+                case "-": return a - b;
+                default:
+                    throw new Exception("未支持运算符");
+            }
+        }
+    }
+}
--- 计算器/Form1.cs
+++ 计算器/Form1.cs
@@ -17,84 +17,6 @@ namespace 计算器
             InitializeComponent();
         }
 
-        /// <summary>
-        /// 文本解析
-        /// </summary>
-        /// <param name="currentInput">当前输入字符 一般为当前触发按键</param>
-        public void TextInput(string currentInput)
-        {
-            string text = "";
-            foreach (KeyValueModel keyValue in keyValuePairs)
-            {
-                text += keyValue.ToString();
-            }
-
-            if (currentNum != 0)
-            {
-                text += currentNum.ToString();
-            }
-            else if (currentNum == 0 && currentInput == "0")
-            {
-                text += "0";
-            }
-
-            if (text == "")
-            {
-                text = "0";
-            }
-
-            TB_1.Text = text;
-        }
-        /// <summary>
-        /// 数值参数
-        /// </summary>
-        /// <param name="input"></param>
-        public void HandleInput(string input)
-        {
-            switch (input)
-            {
-                case "0":
-                case "1":
-                case "2":
-                case "3":
-                case "4":
-                case "5":
-                case "6":
-                case "7":
-                case "8":
-                case "9":
-                    currentNum = currentNum * 10 + Convert.ToInt64(input);
-                    break;
-                case "+":
-                case "-":
-                case "*":
-                case "/":
-                    HandleSymbol(input);
-                    break;
-                default:
-                    break;
-            }
-            TextInput(input);
-            beforeInput = input;
-        }
-
-        /// <summary>
-        /// 字符参数
-        /// </summary>
-        /// <param name="symbol"></param>
-        private void HandleSymbol(string symbol)
-        {
-            if ((beforeInput == "+" || beforeInput == "-" || beforeInput == "*" || beforeInput == "/") && currentNum == 0)
-            {
-                keyValuePairs.Last().Key = symbol;
-            }
-            else
-            {
-                keyValuePairs.Add(new KeyValueModel() { Key = symbol, Value = currentNum });
-                currentNum = 0;
-            }
-        }
-
         private void Button_1_Click(object sender, EventArgs e) => HandleInput(((Button)sender).Text);
 
         private void Button_2_Click(object sender, EventArgs e) => HandleInput(((Button)sender).Text);
@@ -187,6 +109,7 @@ namespace 计算器
         private void Button_Multi_Click(object sender, EventArgs e) => HandleInput(((Button)sender).Text);
 
         private void Button_Dev_Click(object sender, EventArgs e) => HandleInput(((Button)sender).Text);
+        
         /// <summary>
         /// 实现四则运算
         /// </summary>
@@ -238,30 +161,7 @@ namespace 计算器
             TB_1.Text = currentNum.ToString();
         }
 
-        /// <summary>
-        /// 四则运算处理
-        /// </summary>
-        /// <param name="symbol"></param>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <returns></returns>
-        /// <exception cref="Exception"></exception>
-        private long Eq(string symbol, long a, long b)
-        {
-            if (symbol == "/" && b == 0)
-            {
-                throw new Exception("不能除0!");
-            }
-            switch (symbol)
-            {
-                case "*": return a * b;
-                case "/": return a / b;
-                case "+": return a + b;
-                case "-": return a - b;
-                default:
-                    throw new Exception("未支持运算符");
-            }
-        }
+
     }
 }
 
